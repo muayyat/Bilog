c4421cbd90385a4f19e8d06089e4d70d53fe461f
Muayyat Billah <muayyat@outlook.com>
2020-04-20 02:20:50 +0100

    Add files via upload

---
+++ Classes/SearchState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bilog
+{
+    public class SearchState
+    {
+        public int CurrentPage { get; set; }
+        public string CurrentCategoryID { get; set; }
+    }
+}
